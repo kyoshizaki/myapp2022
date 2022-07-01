cd0b7658ce22f2e454485bb9c00cd82c49a25382
Koichi Yoshizaki <kyoshi@oita-u.ac.jp>
2022-07-01 03:44:45 +0000

    Lesson12の課題

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     resources :posts do
-      resource :comments
+      resources :comments
     end
     
     root 'posts#index'
 end
+
+
